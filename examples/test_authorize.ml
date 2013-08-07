138c36afe5d36d99e7cfd081797617be7892273f
Chua Jie Sheng <chuajiesheng@gmail.com>
2013-08-08 03:13:00 +0800

    Completed example authorize; Add example html to demo Example require active domain to work; Moving to next testing stage

--- examples/test_authorize.ml
+++ examples/test_authorize.ml
@@ -6,12 +6,18 @@ let scope = Js.array scope_a
 
 let m = Auth.empty_params ()
 
-let _ = m##client_id <- client_id
-let _ = m##immediate <- Js._true
-let _ = m##response_type <- response_type
-let _ = m##scope <- scope
+let _ = m##client_id_ <- client_id
+let _ = m##immediate_ <- Js._true
+let _ = m##response_type_ <- response_type
+let _ = m##scope_ <- scope
 
 let login_completed (t : Token.oauth_token) =
+  Dom_html.window##alert(Js.string "hello~");
   ignore(Js._true)
 
-let _ = Auth.authorize m (Js.wrap_callback login_completed)
+let start _ =
+  Auth.authorize m (Js.wrap_callback login_completed);
+  Dom_html.window##alert(Js.string "authorize called~");
+  Js._true
+
+let _ = Dom_html.window##onload <- Dom_html.handler start
