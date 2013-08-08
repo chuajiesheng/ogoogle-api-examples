9adad99a378c5db0a2b7a0b31358f55d7c18d1ec
Chua Jie Sheng <chuajiesheng@gmail.com>
2013-08-09 01:03:54 +0800

    Working authorize example This work on chuajiesheng/ogoogle-api@8d18a32501a8b347a33444ba1d782dd22ba62094

--- examples/test_authorize.ml
+++ examples/test_authorize.ml
@@ -1,21 +1,22 @@
 let client_id = Js.string "111026527826"
-let immediate = Js._false
-let response_type = Js.string "token"
 let scope_a = [| Js.string "https://www.googleapis.com/auth/plus.me" |]
 let scope = Js.array scope_a
+let api_key = Js.string "AIzaSyAbB4Mr1ilCJX8MFZKc2qXeY5AIJTXEOo0"
 
 let m = Auth.empty_params ()
 
 let _ = m##client_id_ <- client_id
-let _ = m##immediate_ <- immediate
-let _ = m##response_type_ <- response_type
 let _ = m##scope_ <- scope
 
-let login_completed (t : Token.oauth_token) =
-  Dom_html.window##alert(Js.string "hello~");
-  ignore(Js._true)
+let login_completed (t : Token.oauth_token Js.t) =
+  Dom_html.window##alert(Js.string "callback~");
+  Firebug.console##log_2(Js.string "[access token]", t##access_token_);
+  Firebug.console##log_2
+    (Js.string "[gapi.auth.getToken()]",
+     (Js.Unsafe.fun_call (Js.Unsafe.variable "gapi.auth.getToken") [||]))
 
 let start _ =
+  Auth.set_api_key api_key;
   Auth.authorize m (Js.wrap_callback login_completed);
   Dom_html.window##alert(Js.string "authorize called~");
   Js._true
