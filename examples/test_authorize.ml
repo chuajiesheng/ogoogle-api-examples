d7bf3b97834958644d92ea2dbe176054423053c4
Chua Jie Sheng <chuajiesheng@gmail.com>
2013-08-09 17:33:59 +0800

    Add gapi.auth.init; Change output to console

--- examples/test_authorize.ml
+++ examples/test_authorize.ml
@@ -9,16 +9,22 @@ let _ = m##client_id_ <- client_id
 let _ = m##scope_ <- scope
 
 let login_completed (t : Token.oauth_token Js.t) =
-  Dom_html.window##alert(Js.string "callback~");
+  Firebug.console##log(Js.string "callback~");
   Firebug.console##log_2(Js.string "[access token]", t##access_token_);
   Firebug.console##log_2
     (Js.string "[gapi.auth.getToken()]",
      (Js.Unsafe.fun_call (Js.Unsafe.variable "gapi.auth.getToken") [||]))
 
-let start _ =
+let initalized () =
   Auth.set_api_key api_key;
+  Firebug.console##log(Js.string "gapi.client.setApiKey called");
   Auth.authorize m (Js.wrap_callback login_completed);
-  Dom_html.window##alert(Js.string "authorize called~");
+  Firebug.console##log(Js.string "gapi.auth.authorize called");
+  ()
+
+let start _ =
+  Auth.init (Js.wrap_callback initalized);
+  Firebug.console##log(Js.string "gapi.auth.init called");
   Js._true
 
 let _ = Dom_html.window##onload <- Dom_html.handler start
